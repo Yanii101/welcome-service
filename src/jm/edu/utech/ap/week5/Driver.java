2e5e7f7796c5b5f66986265b0a731500bfa8b6eb
1500625 <1500625@LABCWS-14.STUDENTS.utech.edu.jm>
2018-09-27 14:09:47 -0500

    added driver

---
+++ src/jm/edu/utech/ap/week5/Driver.java
@@ -0,0 +1,8 @@
+package jm.edu.utech.ap.week5;
+
+public class Driver implements Driveble {
+public void drive(float numberofMiles)
+{
+	System.out.println(String.format("yaneeke has driven"+ "%.2f miles", numberofMiles));
+}
+}
